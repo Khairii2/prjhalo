8badfc7dd34b4a807786cf330b10658d5c64c2c9
Khairii2 <jw.miul28@gmail.com>
2025-05-29 12:04:36 +0800

    memasukkan data 5 buah nama ke dalam array string

--- hallo.cpp
+++ hallo.cpp
@@ -5,6 +5,14 @@ using namespace std;
 int main() 
 {
     string nama[5];  //inisialisasi array string untuk menampung 5 nama
-                     
+                  
+    //memasukkan data 5 buah nama ke dalam array string
+    cout << "Silahkan Masukkan 5 Nama:\n";
+    for (int i = 0; i < 5; i++) 
+    {
+        cout << "Nama ke-" << i + 1 << ": ";
+        getline(cin, nama[i]);
+    }
+
     return 0;
 }

84c4f6dbc0fb38cab2d1eabc8576a8bf65f0fcd9
Khairii2 <jw.miul28@gmail.com>
2025-05-29 23:16:29 +0800

    menampilkan data pada array

--- hallo.cpp
+++ hallo.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() 
 {
     string nama[5];  //inisialisasi array string untuk menampung 5 nama
-                  
+                 
     //memasukkan data 5 buah nama ke dalam array string
     cout << "Silahkan Masukkan 5 Nama:\n";
     for (int i = 0; i < 5; i++) 
@@ -13,6 +13,16 @@ int main()
         cout << "Nama ke-" << i + 1 << ": ";
         getline(cin, nama[i]);
     }
+    
+    //menampilkan data pada array
+    cout << "\n======== DAFTAR NAMA ========\n";
+    for (int i = 0; i < 5; i++) 
+    {
+        cout << i + 1 << ". " << nama[i] << " " << endl;
+    }
 
-    return 0;
+   cout << "\nMantap! Total ada " << 5 << " nama yang berhasil disimpan.\n";
+   cout << "Terima kasih sudah memasukkan data\n";
+
+   return 0;
 }
